d3c90dba9e46b8f8fb764cc01dfdf5d103a25670
minaljibhkate <144208895+Minal23448@users.noreply.github.com>
2025-06-01 20:41:06 +0530

    Update py.py

--- py.py
+++ py.py
@@ -1,1 +1,9 @@
 print ("hello")
+H = int(input("ENTER THE HEIGHT: "))
+W = int(input("ENTER THE WIDTH: "))
+A = H*W
+
+if H >= 0 and W >=0:
+    print("Area of the rectangle is ",A)
+else:
+    print("INVALID INOUT")
